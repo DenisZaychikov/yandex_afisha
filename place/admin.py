97c31c09842b618b84f5699ff9dee8d9d58a2ddf
DenisZaychikov <denis.zaj4ikov@yandex.ru>
2020-10-22 15:08:25 +0300

    Use html_format instead of mark_safe

--- place/admin.py
+++ place/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from .models import Place, Image
-from django.utils.safestring import mark_safe
+from django.utils.html import format_html
 from adminsortable2.admin import SortableInlineAdminMixin
 
+IMAGE_WIDTH = 200
 
 @admin.register(Image)
 class AdminImage(admin.ModelAdmin):
@@ -10,7 +11,7 @@ class AdminImage(admin.ModelAdmin):
     raw_id_fields = ('place',)
 
     def get_preview(self, obj):
-        return mark_safe(
+        return format_html(
             '<img src="{url}" width="{width}" height={height} />'.format(
                 url=obj.image.url,
                 width=obj.image.width,
@@ -24,10 +25,10 @@ class ImageInLine(SortableInlineAdminMixin, admin.TabularInline):
     readonly_fields = ["get_preview"]
 
     def get_preview(self, obj):
-        return mark_safe(
+        return format_html(
             '<img src="{url}" width="{width}" />'.format(
                 url=obj.image.url,
-                width=200,
+                width=IMAGE_WIDTH,
             )
         )
 
